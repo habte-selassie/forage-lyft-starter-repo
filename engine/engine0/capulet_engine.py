f8448e5d136254bcd540c533f5d7994f99c4ca17
Habte Selassie <habtuchelsea1000@gmail.com>
2023-11-30 22:11:02 +0300

    refactor the sternaman engine

--- engine/engine0/capulet_engine.py
+++ engine/engine0/capulet_engine.py
@@ -1,6 +1,5 @@
 from engine import Engine
 
-
 class CapuletEngine(Engine):
     def __init__(self, last_service_date, current_mileage, last_service_mileage):
         super().__init__(last_service_date)
